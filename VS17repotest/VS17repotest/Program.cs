2adde4ac05dca4daa079812dda07ec36428cdcc3
Your Name <ferdinand.bauer@fh-erfurt.de>
2017-03-13 16:45:37 +0100

    add fleck

--- VS17repotest/VS17repotest/Program.cs
+++ VS17repotest/VS17repotest/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Fleck;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VS17repotest
@@ -10,7 +12,20 @@ namespace VS17repotest
     {
         static void Main(string[] args)
         {
-            int a = 0;
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = message => socket.Send(message);
+            });
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
         }
+
+        
     }
 }
